b88a0470191ef0c537efbb4c0d2bca99437f4157
Jared <68973848+JRH89@users.noreply.github.com>
2023-01-28 19:26:44 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
+use strict;
 // Setup
 
 const scene = new THREE.Scene();
